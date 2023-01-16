9dd29ae28cd53a5142be759ef74dd13860deed17
hellogyeol <hellogyeol@gmail.com>
2023-01-16 22:26:59 +0900

    test: POST 요청에 대한 데이터 반환 테스트

--- server.js
+++ server.js
@@ -7,19 +7,44 @@ const { MongoClient } = require("mongodb");
 const url = process.env.DB_URL;
 const client = new MongoClient(url);
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
-  app.get('/todolist', (req, res) => {
-    async function getTodoList() {
-      await client.connect();
-      const col = client.db('csrDb').collection('csrCol');
-  
-      const todoList = await col.find({}).toArray();
-      console.log(todoList);
-      res.send(todoList);
-    }
-    getTodoList();
-  });
   res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get('/todolist', (req, res) => {
+  async function getTodoList() {
+    await client.connect();
+    const col = client.db('csrDb').collection('csrCol');
+
+    const todoList = await col.find({}).toArray();
+    console.log(todoList);
+    res.send(todoList);
+  }
+  getTodoList();
+});
+
+app.post('/todolist', (req, res) => {
+  async function postTodoList() {
+    await client.connect();
+    const col = client.db('csrDb').collection('csrCol');
+
+    const content = req.body.content;
+    await col.insertOne({
+      content: content
+    });
+
+    const todoList = await col.find({}).toArray();
+    console.log(todoList);
+    res.send(todoList);
+  }
+  postTodoList();
+  
+  app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+  });
 });
 
 app.listen(port, () => {
