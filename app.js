205fc91a1bb7dbae237bed17a548ade7babb6ba1
hellogyeol <hellogyeol@gmail.com>
2023-01-24 23:31:33 +0900

    feat: Todo 출력 및 추가(새로고침X)

--- app.js
+++ app.js
@@ -14,49 +14,37 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+app.get('/list', (req, res) => {
+  getList();
+  async function getList() {
+    await client.connect();
+    const col = client.db('csrDb').collection('csrCol');
+    // await col.deleteMany({})
+    const todoList = await col.find({}).toArray();
+    console.log(todoList);
 
-app.get('/test', (req, res) => {
-  console.log(req.body)
-  res.send('get msg');
+    res.send(todoList);
+  }
 });
-app.post('/test', (req, res) => {
-  console.log(req.body.wow)
-  res.send('post msg');
+
+app.post('/list', (req, res) => {
+  postList();
+  async function postList() {
+    await client.connect();
+    const col = client.db('csrDb').collection('csrCol');
+
+    await col.insertOne({
+      id: String(Date.now()),
+      content: req.body.content,
+      done: false
+    })
+
+    const todoList = await col.find({}).toArray();
+    console.log(todoList);
+
+    res.send(todoList);
+  }
 });
-
-
-// app.get('/todolist', (req, res) => {
-//   getTodoList();
-//   async function getTodoList() {
-//     await client.connect();
-//     const col = client.db('csrDb').collection('csrCol');
-
-//     const todoList = await col.find({}).toArray();
-//     console.log(todoList);
-//     console.log(req.body)
-
-//     res.send(todoList);
-//   }
-// });
-
-// app.post('/todolist', (req, res) => {
-//   postTodoList();
-//   async function postTodoList() {
-//     await client.connect();
-//     const col = client.db('csrDb').collection('csrCol');
-
-//     await col.insertOne({
-//       id: String(Date.now()),
-//       content: req.body.content,
-//       done: false
-//     })
-
-//     const todoList = await col.find({}).toArray();
-//     console.log(todoList);
-
-//     res.send(todoList);
-//   }
-// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
